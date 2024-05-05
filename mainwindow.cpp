c6126437c7c4d9b6b4994ae01cc95d22991eccc8
Amirshayan1 <sh.tatari18@gmail.com>
2024-05-05 21:17:59 +0200

    added window member variable

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,8 @@
 #include "mainwindow.hpp"
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent), ui(new Ui::MainWindow), _window("player window", this)
 {
     ui->setupUi(this);
 }
--- mainwindow.hpp
+++ mainwindow.hpp
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
+#include "widgets/window.hpp"
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -10,12 +11,11 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    Ui::MainWindow *ui;
+    Window _window;
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
+    ~MainWindow(); 
 };
 #endif // MAINWINDOW_HPP
