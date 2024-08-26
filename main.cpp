f1fed7c8d75ba147ac322d0a602f2e124b6b33b6
Amirshayan1 <sh.tatari18@gmail.com>
2024-08-26 18:45:37 +0200

    added exception handler

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    try{
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+        return a.exec();
+    } catch(const std::exception& exp){
+        std::cout << "main: ERROR: " << exp.what() << '\n';
+    }
 }
